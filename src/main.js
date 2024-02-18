25d07d7df0b0ebfcd23b97e7293d750e5b830394
Gurur Kısaoğlu <108701457+justwantajob@users.noreply.github.com>
2024-02-18 13:12:20 +0300

    version 1
    pushed to the github

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/loadout.css';
+import '../styles/components/projects.css';
+import '../styles/utils.css';
